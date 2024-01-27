e51bff2d40192c7f53b1686a10308bee8436b05f
Rusllxn <rusllxn@gmail.com>
2024-01-28 04:18:37 +0600

    done

---
+++ Homework 5/Models/UserCard.swift
@@ -0,0 +1,16 @@
+//
+//  UserCard.swift
+//  Homework 5
+//
+//  Created by Руслан Канжарбеков on 27.01.2024.
+//
+
+import Foundation
+import UIKit
+
+struct UserCard {
+    let avatarImage: UIImage?
+    let notificationText: String
+    let timestampText: String
+    let productImage: UIImage?
+}
